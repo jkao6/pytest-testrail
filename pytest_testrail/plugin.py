54fa0d26f0a512bacb7a897e4fd0aaaa31058b96
Jason Kao <jkao@corp.zeromotorcycles.com>
2022-05-23 15:45:55 -0700

    added duration=result.get('duration')

--- pytest_testrail/plugin.py
+++ pytest_testrail/plugin.py
@@ -354,8 +354,9 @@ class PyTestRailPlugin(object):
             #if duration:
             #    duration = 1 if (duration < 1) else int(round(duration))  # TestRail API doesn't manage milliseconds
             #    entry['elapsed'] = str(duration) + 's'
-            if result.get('duration'):
-                duration = 1 if (int(result.get('duration')) < 1) else int(round(duration))  # TestRail API doesn't manage milliseconds
+            duration = result.get('duration')
+            if duration:
+                duration = 1 if (duration < 1) else int(round(duration))  # TestRail API doesn't manage milliseconds
                 entry['elapsed'] = str(duration) + 's'
             if self.custom_dut:
                 entry['custom_dut'] = self.custom_dut
