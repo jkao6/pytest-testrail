43de17dead9c052e49ddd5cc86452ff0b8a16e20
Jason Kao <jkao@corp.zeromotorcycles.com>
2022-05-23 12:40:53 -0700

    fixed syntax

--- pytest_testrail/plugin.py
+++ pytest_testrail/plugin.py
@@ -334,7 +334,7 @@ class PyTestRailPlugin(object):
 
         # Publish results
         #Make sure file exists before getting the string data
-        if os.path.exists.("tr_log.txt") == True:
+        if os.path.exists("tr_log.txt") == True:
             # import text file with results
             log_file = open("tr_log.txt")
         data = {'results': []}
@@ -357,7 +357,7 @@ class PyTestRailPlugin(object):
                 entry['custom_dut'] = self.custom_dut
             data['results'].append(entry)
         # close the file and delete it
-        if os.path.exists.("tr_log.txt") == True:
+        if os.path.exists("tr_log.txt") == True:
             log_file.close()
             subprocess.check_output("del /f tr_log.txt")
                 
